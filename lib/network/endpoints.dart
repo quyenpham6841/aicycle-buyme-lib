f8abec83dc31577adaf4e118d6ae2d8497ec48c3
Quyền <76800542+quyenpham6841@users.noreply.github.com>
2023-12-06 11:10:23 +0700

    Update endpoints.dart

--- lib/network/endpoints.dart
+++ lib/network/endpoints.dart
@@ -1,8 +1,4 @@
 abstract class BaseEndpoint {
-  // static String get baseUrl => 'https://dev-api-insurance.aicycle.ai';
-  // static String get adminBaseUrl => 'https://dev-api-admin.aicycle.ai';
-  // static String get baseUrl => 'https://stage-api-insurance.aicycle.ai';
-  // static String get adminBaseUrl => 'https://stage-api-admin.aicycle.ai';
   static String get baseUrl => 'https://api-aws-insurance.aicycle.ai';
   static String get adminBaseUrl => 'https://api-aws-admin.aicycle.ai';
 }
@@ -13,7 +9,6 @@ abstract class Endpoint {
   static String get getImageUploadUrl => '/images/url';
   static String get validateUpload => '/claimimages/validate';
   static String get claimFolders => '/claimfolders';
-  // static String get getImageInformation => '/insurance/claimFolder/{claimId}';
   static String get deleteImageById => '/claimimages/{imageId}';
   static String get getImageInformation =>
       '/v2/claimfolders/{claimId}/insurance-images';
